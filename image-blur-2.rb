0195e2154eb9c86bb2aad69d45e0699823c7490b
Alisonhardison <appletini19@gmail.com>
2019-09-16 18:01:03 +0000

    updated image blur 2

--- image-blur-2.rb
+++ image-blur-2.rb
@@ -1,50 +1,61 @@
-class Image
-      attr_accessor :image
+class Image  #build a class that allows for building a new image with specified data
+  attr_accessor :rows # makes rows readable and writable to user
 
-          def initialize(image)
-            @image = image
-          end
+  def initialize(rows)
+    @rows = rows      #is storing the data at variable rows
+  end
 
-              def output_image
-                @image.each do |row|
-                  puts row.join
-              end
+  def output_image      # take each row join together output image
+    @rows.each do |row|
+      puts row.join
+    end
+  end
 
+  def blur (distance = 1)     #find the pixels to blur in the array
+    distance.times do         #then calls the blur coords method
+      blur_coordinates
+    end
+  end
+
+
+  def blur_coordinates
+    blur_coordinates = [ ]      #creates an empty array
+    @rows.each_with_index do |row, row_index|     #loop through each row and store the varible data
+      row.each_with_index do |column, column_index| #loop through each column and store the variable data
+        blur_coordinates << [row_index, column_index] if column == 1    #if column is 1 push blurcoords on row_index/column_index
       end
-
-                  def blur (distance = 1)
-                    distance.times do
-                      blur_coordinates
-                    end
-                  end
-      
-
-                      def blur_coordinates
-                        blur_coordinates = [ ]
-                          @image.each_with_index do |row, i|
-                            row.each_with_index do |x, row_i|
-                              blur_coordinates << [i, row_i] if x == 1
-                        end
-                      end
+    end
 
 
-                          blur_coordinates.each do |coordinates|
-                            @image[coordinates[0]][coordinates[1]+1]=1 if coordinates[1]+1 <= @image[coordinates[0]].length - 1
-                            @image[coordinates[0]][coordinates[1]-1]=1 if coordinates[1]-1 >= 0
-                            @image[coordinates[0]+1][coordinates[1]]=1 if coordinates[0]+1 <= @image.length - 1
-                            @image[coordinates[0]-1][coordinates[1]]=1 if coordinates[0]-1 >= 0
-                            end
-                          end
+    blur_coordinates.each do |coordinates|
+      row_coord = coordinates[0]  #varible to hold sub[0]
+      column_coord = coordinates[1]     #varible to hold[1]
+      @rows[row_coord][column_coord+1]=1 unless column_coord >= @rows[row_coord].length - 1    #right  
+      @rows[row_coord][column_coord-1]=1 unless column_coord == 0                              #left
+      @rows[row_coord+1][column_coord]=1 unless row_coord+1 >= @rows.length - 1                  #down
+      @rows[row_coord-1][column_coord]=1 unless row_coord == 0                                 #up
+    end
+  end
 
 end
 
 
 image = Image.new([
 [0, 0, 0, 0],
-[0, 1, 0, 0],
-[0, 0, 0, 1],
+[0, 1, 0, 0], #[1,1]
+[0, 0, 0, 1], #[2,3]
 [0, 0, 0, 0]
 ])
 
 image.blur
 image.output_image
+
+image2 = Image.new([
+[0, 0, 0, 0],
+[0, 1, 0, 0],
+[0, 0, 0, 1],
+[0, 0, 0, 0]
+])
+
+image2.blur
+image2.output_image
